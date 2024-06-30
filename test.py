561a18457c7bd37ca1249f560d21ae8751108f3e
F-Depi <fdp.federico@gmail.com>
2024-06-30 14:47:13 +0200

    categorie e codici gare

--- test.py
+++ test.py
@@ -1,17 +1,27 @@
 import pandas as pd
+import json
 from my_functions import *
 
-anno = '2021'
+
+anno = '2024'
 tipo_att = 'P'
 sesso = 'M'
 cat = 'X'
-gara = '03'
+gara = '110 Hs h106-9.14'
 tip_estr = '1'
 vento = '2'
 regione = '0'
 naz = '2'
 lim = '5'
 societa = ''
+
+with open('dizionario_gare.json') as f:
+    dict_gare = json.load(f)
+
+for gara in dict_gare:
+    print(gara)
+
+exit()
 
 file = 'test.csv'
 
