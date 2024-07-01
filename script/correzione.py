119cd4de58906927306f632c884fa92bcb720e4b
F-Depi <fdp.federico@gmail.com>
2024-07-01 17:00:51 +0200

    corretto i dati

--- script/correzione.py
+++ script/correzione.py
@@ -1,27 +1,26 @@
 import pandas as pd
 import os
 
-folder = '../database/outdoor/'
-for sub_folder in os.listdir(folder):
+folder = '../database/indoor/'
+log_file = folder + 'errori.txt'
+write_header = True
 
+for sub_folder in os.listdir(folder):
+    if sub_folder.endswith('.txt'):
+        continue
     sub_folder = sub_folder + '/'
 
-    log_file = folder + sub_folder + 'errori.txt'
-    with open(log_file, 'w') as f_log:
+    for file in os.listdir(folder + sub_folder):
+        if file.endswith('.csv'):
+            print(file)
 
-        write_header = True
-
-        for file in os.listdir(folder + sub_folder):
-            if file.endswith('.csv'):
-                print(file)
-
-                df = pd.read_csv(folder + sub_folder + file, sep=',', header=0, dtype=str)
-                
-                gara = file[:-20]
-                if 'cronometraggio' in df.columns:
-                    df_typo = df[df['cronometraggio'] == '2.0']
-                    df_typo['gara'] = gara
-                    df_typo.to_csv(folder + sub_folder + log_file, sep=',', mode='a',index=False, header=write_header)
-                    write_header = False
+            df = pd.read_csv(folder + sub_folder + file, sep=',', header=0, dtype=str)
+            
+            gara = file[:-20]
+            if 'cronometraggio' in df.columns:
+                df_typo = df[df['cronometraggio'] == '2.0']
+                df_typo.insert(0, 'gara', gara)
+                df_typo.to_csv(log_file, sep=',', mode='a',index=False, header=write_header)
+                write_header = False
 
 
