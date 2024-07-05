e58eb93d493da08cb3b850bb097b10cec9cdcda4
F-Depi <fdp.federico@gmail.com>
2024-07-05 14:18:06 +0200

    corretto il miglio del README

--- script/correzione.py
+++ script/correzione.py
@@ -3,7 +3,7 @@ import os
 import json
 from my_functions import *
 
-''' Trova gli errori e li aggiunge al file di log
+''' Trova gli errori e li aggiunge al file di log '''
 folder = '../database/outdoor/'
 log_file = folder + 'errori_now.csv'
 write_header = True
@@ -19,17 +19,18 @@ for sub_folder in os.listdir(folder):
 
             df = pd.read_csv(folder + sub_folder + file, sep=',', header=0, dtype=str)
             
-            gara = file[:-20]
+            gara = file[:-15]
             if 'cronometraggio' in df.columns:
-                df_typo = df[df['cronometraggio'] == '2.0']
+                df_typo = df[df['cronometraggio'] == 'x']
                 df_typo.insert(0, 'gara', gara)
                 df_typo.to_csv(log_file, sep=',', mode='a',index=False, header=write_header)
                 write_header = False
-'''
 
 
 
-''' Cambiamo alcune cose sulla strutta del database '''
+
+
+''' Cambiamo alcune cose sulla strutta del database
 dict_gare = json.load(open('dizionario_gare.json'))
 col_dtype = json.load(open('colonne_dtype.json'))
 
@@ -47,22 +48,5 @@ for file in os.listdir(folder):
     df.to_csv(folder + file, sep=',', index=False)
 
     print('-'*90)
+'''
 
-
-
-
-
-
-
-
-
-exit()
-with open('log', 'w') as f_log:
-    file = '../database/outdoor/Marcia/Marcia_10km_2005_2024-06-29.csv'
-    df = pd.read_csv(file, sep=',', header=0, dtype=str)
-    df['prestazione'] = pd.to_numeric(df['prestazione'])
-    df['cronometraggio'] = pd.to_numeric(df['cronometraggio'])
-    mask = df['cronometraggio'] == 3
-    print(df[mask])
-    df[['prestazione', 'cronometraggio']] = df.apply(lambda row: conversione_manuale_elettrico(row['tempo'], f_log), axis=1, result_type='expand')
-    print(df[mask])
