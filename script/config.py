e7889f65a66ae96d9b671db79b626790cd282aa9
F-Depi <fdp.federico@gmail.com>
2025-04-17 00:20:51 +0200

    We actually transitioned to SQL, it wasn't that hard

---
+++ script/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_CONFIG = {
+    'host': os.getenv('DB_HOST', 'localhost'),
+    'database': os.getenv('DB_NAME', 'athletics_stats'),
+    'user': os.getenv('DB_USER', 'postgres'),
+    'password': os.getenv('DB_PASSWORD', '1234'),
+    'port': '5432'
+}
+
