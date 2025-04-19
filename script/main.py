7b58eec4e0b23bbaf66ad72588ec58632d2c7ffb
F-Depi <fdp.federico@gmail.com>
2025-04-19 16:33:16 +0200

    remove duplicate results of RF due to them believed to be CF by fidal database. Almost ready to scrape U14 results

--- script/main.py
+++ script/main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pandas as pd
 from my_functions import get_data_FIDAL, format_data_FIDAL, ultimo_aggiornamento_FIDAL
 
 '''
@@ -25,61 +26,46 @@ with open('nottata.log', 'w') as f_log:
     lim = '0'
     societa = ''
 
-    dict_gare = json.load(open('dizionario_gare.json'))
+    dict_gare = json.load(open('script/dizionario_gare.json'))
+    dict_reg_prov = json.load(open('script/regioni_province.json'))
 
-    ambiente = 'P'
+    for ambiente, ambiente_folder in zip(['P', 'I'], ['outdoor', 'indoor']):
+        for gara in dict_gare.keys():
+            print('-'*90, file=f_log)
+            print(gara, file=f_log)
+            
+            cod_gara = dict_gare[gara]['codice']
+            tipo_gara = dict_gare[gara]['tipo']
+            
+            folder = f'database/{ambiente_folder}/{tipo_gara}/'
+            if not os.path.exists(folder): os.makedirs(folder)
+            file = folder + gara + '_' + data_agg + '.csv'
+            
+            write_header = True
+            for anno in range(2005, pd.Timestamp.now().year):
+                anno = str(anno)
+                #for cat in ['C', 'X']:
+                #    for sesso in ['M', 'F']:
+            
+                #        regione = 0
+                #        print(anno, cat, sesso, regione, file=f_log)
+            
+                #        df = get_data_FIDAL(anno, ambiente, sesso, cat, cod_gara, tip_estr, vento, regione, naz, lim, societa, f_log)
+                #        df = format_data_FIDAL(df, gara, ambiente, f_log) 
+                #        if df is not None:
+                #            df.to_csv(file, index=False, mode='a', header=write_header)
+                #            write_header = False
 
-    for gara in dict_gare.keys():
-        print('-'*90, file=f_log)
-        print(gara, file=f_log)
-        
-        cod_gara = dict_gare[gara]['codice']
-        tipo_gara = dict_gare[gara]['tipo']
-        
-        folder = '../database/outdoor/' + tipo_gara + '/'
-        if not os.path.exists(folder): os.makedirs(folder)
-        file = folder + gara + '_' + data_agg + '.csv'
-        
-        write_header = True
-        for anno in range(2005, 2025):
-            anno = str(anno)
-            for cat in ['E', 'R', 'C', 'X']:
-                for sesso in ['M', 'F']:
-        
-                    print(anno, cat, sesso, file=f_log)
-        
-                    df = get_data_FIDAL(anno, ambiente, sesso, cat, cod_gara, tip_estr, vento, regione, naz, lim, societa, f_log)
-                    df = format_data_FIDAL(df, gara, ambiente, f_log) 
-                    if df is not None:
-                        df.to_csv(file, index=False, mode='a', header=write_header)
-                        write_header = False
-        
-        
+                for cat in ['E', 'R']:
+                    for sesso in ['M', 'F']:
+                        for reg in dict_reg_prov.keys():
 
-    ambiente = 'I'    
+                            regione = reg
+                            print(anno, cat, sesso, reg, file=f_log)
 
-    for gara in dict_gare.keys():
-        print('-'*90, file=f_log)
-        print(gara, file=f_log)
-        
-        cod_gara = dict_gare[gara]['codice']
-        tipo_gara = dict_gare[gara]['tipo']
-        
-        folder = '../database/indoor/' + tipo_gara + '/'
-        if not os.path.exists(folder): os.makedirs(folder)
-        file = folder + gara + '_' + data_agg + '.csv'
-        
-        write_header = True
-        for anno in range(2005, 2025):
-            anno = str(anno)
-            for cat in ['E', 'R', 'C', 'X']:
-                for sesso in ['M', 'F']:
-        
-                    print(anno, cat, sesso, f_log)
-        
-                    df = get_data_FIDAL(anno, ambiente, sesso, cat, cod_gara, tip_estr, vento, regione, naz, lim, societa, f_log)
-                    df = format_data_FIDAL(df, gara, ambiente, f_log) 
-                    if df is not None:
-                        df.to_csv(file, index=False, mode='a', header=write_header)
-                        write_header = False
+                            df = get_data_FIDAL(anno, ambiente, sesso, cat, cod_gara, tip_estr, vento, regione, naz, lim, societa, f_log)
+                            df = format_data_FIDAL(df, gara, ambiente, f_log) 
+                            if df is not None:
+                                df.to_csv(file, index=False, mode='a', header=write_header)
+                                write_header = False
 
