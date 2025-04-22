db00c7be8f3500e2b4c0991a1005b9b70f72f6aa
F-Depi <fdp.federico@gmail.com>
2025-04-22 23:46:44 +0200

    Sistemato la prestazione dei tempi manuali (tolto il centesimo in più)

--- script/correzione.py
+++ script/correzione.py
@@ -42,4 +42,30 @@ def trova_errori():
                     write_header = False
 
 
-trova_errori()
+"""Sistemiamo il centesimo in più nella prestazione dei tempi manuali"""
+def togli_centesimo():
+    ''' Trova gli errori e li aggiunge al file di log '''
+    folder = '../database-atletica-csv/indoor/'
+
+    for sub_folder in os.listdir(folder):
+        if sub_folder.endswith('.csv'):
+            continue
+        sub_folder = sub_folder + '/'
+
+        for file in os.listdir(folder + sub_folder):
+            if file.endswith('.csv'):
+                print(file)
+
+                df = pd.read_csv(folder + sub_folder + file, sep=',', header=0, dtype=str)
+
+                gara = file[:-15]
+                if 'cronometraggio' in df.columns:
+                    df['prestazione'] = df['prestazione'].astype(float)
+                    ###df.loc[df['cronometraggio'] == 'm', 'prestazione'] -= 0.01 # Ok ha funzionato ora non serve mai più
+                    ## Round to 2 decimal places
+                    df['prestazione'] = df['prestazione'].round(2)
+                    df.to_csv(folder + sub_folder + file, sep=',', index=False, header=True)
+
+
+#trova_errori()
+togli_centesimo()
