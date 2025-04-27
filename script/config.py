786fd26fd6fd091c49c2b841dfbdc7c96a203586
F-Depi <fdp.federico@gmail.com>
2025-04-27 03:09:13 +0200

    Should automate this

--- script/config.py
+++ script/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 DB_CONFIG = {
     'host': os.getenv('DB_HOST', 'localhost'),
-    'database': os.getenv('DB_NAME', 'athletics_stats'),
+    'database': os.getenv('DB_NAME', 'atletica_italiana'),
     'user': os.getenv('DB_USER', 'postgres'),
     'password': os.getenv('DB_PASSWORD', '1234'),
     'port': '5432'
